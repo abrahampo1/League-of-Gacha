c0c6c1eaecb66b989b1f7de98f44600d4d90925b
Abraham Leiro Fernandez <abraham@cpsoftware.es>
2022-04-12 18:48:47 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,200 @@
+function get_int(name, def = 0) {
+  if (localStorage.getItem(name)) {
+    return parseFloat(localStorage.getItem(name))
+  } else {
+    localStorage.setItem(name, def)
+    return def
+  }
+}
+
+function set_int(name, value, holder = '') {
+  $(holder).html(parseFloat(value).toFixed(0))
+  if (localStorage.setItem(name, value)) {
+    return true
+  } else {
+    return false
+  }
+}
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min
+}
+var points = parseInt(get_int('points'))
+
+$('.point-btn').on('click', function (params) {
+  set_int('points', get_int('points') + 1, '.money-holder')
+})
+
+function comprar(item) {
+  let coste = cajas[item]['costo']
+  if (coste <= get_int('points')) {
+    set_int('points', get_int('points') - coste, '.money-holder')
+    let probs = 0
+    cajas[item]['items'].forEach((element) => {
+      probs += element['prob']
+    })
+    let rand = getRandomArbitrary(0, probs)
+    probs = 0
+    let comprado
+    cajas[item]['items'].forEach((element) => {
+      console.log(rand)
+      probs += element['prob']
+      if (rand < probs && comprado == undefined) {
+        comprado = element['articulo']
+      }
+    })
+    swal({
+      text: 'Te ha tocado: ' + comprado['nombre'],
+      icon: comprado['image'],
+    })
+    if (compras[comprado['key']]) {
+      compras[comprado['key']]['nivel'] = compras[comprado['key']]['nivel'] + 1;
+    } else {
+      compras[comprado['key']] = comprado
+    }
+    const jsConfetti = new JSConfetti()
+    jsConfetti.addConfetti()
+    localStorage.setItem('compras', JSON.stringify(compras))
+    print_box()
+  }
+}
+const arts = {
+  Scroom: {
+    costo: 100,
+    dps: 1,
+    mult: 1,
+    nivel: 1,
+    nombre: 'una seta ⭐',
+    key: 'Scroom',
+    image:
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fterrados.es%2Fwp-content%2Fuploads%2F2013%2F12%2Fboletus_edulis.png&f=1&nofb=1',
+  },
+  Furias: {
+    costo: 1000,
+    dps: 20,
+    mult: 1,
+    nivel: 1,
+    image:
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvignette.wikia.nocookie.net%2Frogue-lineage%2Fimages%2Fe%2Fed%2FHaselden.png%2Frevision%2Flatest%3Fcb%3D20191107065431&f=1&nofb=1',
+    nombre: 'una chupada a furias ⭐⭐',
+    key: 'Furias',
+  },
+  Darius: {
+    costo: 10000,
+    dps: 100,
+    nivel: 1,
+    mult: 1,
+    nombre: 'una abuela darius en top ⭐⭐⭐⭐',
+    image:
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.models-resource.com%2Fresources%2Fbig_icons%2F7%2F6694.png&f=1&nofb=1',
+    key: 'Darius',
+  },
+}
+
+const cajas = {
+  noob: {
+    nombre: 'Cajita de carton',
+    costo: 100,
+    items: [
+      { articulo: arts['Scroom'], prob: 90 },
+      { articulo: arts['Furias'], prob: 10 },
+    ],
+    image:
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fbasicos.empack.mx%2Fwp-content%2Fuploads%2F2019%2F10%2FCAJA-RM-35-CON-JUEGO-DE-DIVISIONES.png&f=1&nofb=1',
+  },
+  hextech: {
+    nombre: 'Cofre Hextech',
+    costo: 3000,
+    items: [
+      { articulo: arts['Scroom'], prob: 10 },
+      { articulo: arts['Furias'], prob: 70 },
+      { articulo: arts['Darius'], prob: 20 },
+    ],
+    image:
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.masarmadura.com%2Fwp-content%2Fuploads%2F2020%2F12%2Fcajitalol-01.png&f=1&nofb=1',
+  },
+}
+if (localStorage.getItem('compras')) {
+  var compras = localStorage.getItem('compras')
+  compras = JSON.parse(compras)
+} else {
+  var compras = {}
+}
+
+setInterval(() => {
+  let dps = 0
+  Object.entries(compras).forEach(([key, value]) => {
+    dps += value['dps'] * value['nivel']
+    set_int(
+      'points',
+      get_int('points') + (value['dps'] * value['nivel']) / 10,
+      '.money-holder',
+    )
+  })
+  $('.dps-holder').html('DPS: ' + dps)
+}, 100)
+
+function print_shop(params) {
+  $('#shop').html('')
+  Object.entries(arts).forEach(([key, value]) => {
+    let coste = value['costo']
+    let lvl = value['nivel']
+    let images = ''
+    if (compras[key]) {
+      coste = coste * compras[key]['nivel']
+      lvl = compras[key]['nivel']
+    }
+    if (value['image']) {
+      for (let index = 1; index < lvl; index++) {
+        images += '<img src="' + value['image'] + '">'
+      }
+    }
+    $('#shop').append(`
+    <div class="shop-btn" onclick="comprar('${key}')">
+          <div class="shop-data">
+          
+          <div class="costo">
+            ${coste}
+            <img
+              src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvignette.wikia.nocookie.net%2Frogue-lineage%2Fimages%2F7%2F7d%2FSilver_Screenshot-0.png%2Frevision%2Flatest%3Fcb%3D20190613225622&f=1&nofb=1"
+              alt=""
+            />
+          </div>
+          Comprar ${value['nombre']}
+          
+          </div>
+          <div class="shop-images">
+          ${images}
+          </div>
+        </div>
+    
+    `)
+  })
+}
+
+function print_box(params) {
+  $('#shop').html('')
+  Object.entries(cajas).forEach(([key, value]) => {
+    $('#shop').append(`
+    <div class="shop-btn" onclick="comprar('${key}')">
+          <div class="shop-data">
+          
+          <div class="costo">
+            ${value['costo']}
+            <img
+              src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvignette.wikia.nocookie.net%2Frogue-lineage%2Fimages%2F7%2F7d%2FSilver_Screenshot-0.png%2Frevision%2Flatest%3Fcb%3D20190613225622&f=1&nofb=1"
+              alt=""
+            />
+          </div>
+          ${value['nombre']}
+          
+          </div>
+          <div class="shop-icon">
+            <img src="${value['image']}">
+          </div>
+        </div>
+    
+    `)
+  })
+}
+
+print_box()
